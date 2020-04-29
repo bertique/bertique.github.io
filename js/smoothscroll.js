f85517f83a8e2ab5581462a53114af40db6f0af8
Michael Dick <b.michael.dick@gmail.com>
2020-04-29 18:20:29 -0400

    Fix modal pop ups

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,7 +1,7 @@
 // Taken from https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
 var $root = $('html, body');
 
-$('a[href^="#"]').click(function () {
+$('a[href^="#"]').not('a[href^="#my"]').click(function () {
     $root.animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
